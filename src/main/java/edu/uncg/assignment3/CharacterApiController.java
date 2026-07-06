708b129b193ea4c63995b3efcf173f85815c3ba5
jmandevel <newjourneyman@yahoo.com>
2026-07-05 22:47:02 -0400

    Fix character controller request mapping
    should be characters not posts

--- src/main/java/edu/uncg/assignment3/CharacterApiController.java
+++ src/main/java/edu/uncg/assignment3/CharacterApiController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/characters")
 public class CharacterApiController {
 
     private final CharacterService characterService;
