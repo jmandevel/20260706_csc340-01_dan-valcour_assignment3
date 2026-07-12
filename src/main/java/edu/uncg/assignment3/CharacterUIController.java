abeab634afe716c6aaaa0a591a08f8b2a2cfbf82
jmandevel <newjourneyman@yahoo.com>
2026-07-12 15:54:41 -0400

    Add loading of character-details page
    Image loading is not working for now

--- src/main/java/edu/uncg/assignment3/CharacterUIController.java
+++ src/main/java/edu/uncg/assignment3/CharacterUIController.java
@@ -1,7 +1,9 @@
 package edu.uncg.assignment3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
@@ -10,9 +12,18 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/characters")
 @AllArgsConstructor
 public class CharacterUIController {
+    CharacterService characterService;
 
     @GetMapping({"/about", "/about/"})
     public String about() {
         return "about";
+    }
+
+    @GetMapping({"/{characterId}", "/{characterId}/"})
+    public String getPostById(@PathVariable Long characterId, Model model) {
+        Character character = this.characterService.getCharacterById(characterId);
+        model.addAttribute("character", character);
+        model.addAttribute("pageTitle", "Character # " + characterId + " Details");
+        return "character-details";
     }
 }
